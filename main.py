39e10492737921d123781c6ac6e7f68d907d2fdd
s4b7r <github@s4b7r.de>
2021-06-02 12:35:54 +0200

    Implement dot dragging in plot

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 _drag_data = {"x": 0, "y": 0, "item": None}
 
 
+def find_closest_dot(event_x, dots_xs):
+    closest_diff = np.inf
+    closest_dot = None
+    for dot, dot_x in enumerate(dots_xs):
+        xdiff = abs(dot_x - event_x)
+        if xdiff < closest_diff:
+            closest_diff = xdiff
+            closest_dot = dot
+    return closest_dot
+
+
 def drag_start(event):
     """Begining drag of an object"""
     # record the item and its location
-    _drag_data["item"] = coil_canvas.find_closest(event.x, event.y)[0]
-    _drag_data["x"] = event.x
-    _drag_data["y"] = event.y
+    _drag_data["item"] = find_closest_dot(event.xdata, dots_xs)
+    _drag_data["x"] = event.xdata
+    _drag_data["y"] = event.ydata
+    print(f'drag_start {_drag_data}')
 
 
 def drag_stop(event):
@@ -27,55 +39,57 @@ def drag_stop(event):
     _drag_data["item"] = None
     _drag_data["x"] = 0
     _drag_data["y"] = 0
+    print(f'drag_stop {_drag_data}')
 
 
 def drag(event):
     """Handle dragging of an object"""
-    # compute how much the mouse has moved
-    delta_x = event.x - _drag_data["x"]
-    delta_y = 0
-    # move the object the appropriate amount
-    coil_canvas.move(_drag_data["item"], delta_x, delta_y)
-    # record the new position
-    _drag_data["x"] = event.x
-    _drag_data["y"] = event.y
+    if _drag_data['item'] is not None:
+        # compute how much the mouse has moved
+        delta_x = event.xdata - _drag_data["x"]
+        delta_y = 0
+        # move the object the appropriate amount
+        dots_xs[_drag_data['item']] += delta_x
+        replot_dots(dots_xs)
+        # record the new position
+        _drag_data["x"] = event.xdata
+        _drag_data["y"] = event.ydata
+        print(f'drag {_drag_data}')
 
 
 root = Tk()
 root.bind('<Escape>', quit)
 
 fig = Figure(figsize=(5, 4), dpi=100)
-figgrid = fig.add_gridspec(5, 1)
-magplot = fig.add_subplot(figgrid[:-1, 0])
-# magplot.set_title('[0, :-1]')
-coilplot = fig.add_subplot(figgrid[-1:, 0])
-# coilplot.set_title('[0, -1:]')
-
-magplot.plot(x := np.linspace(-1, 1, 50), np.sin(x))
+axes = fig.subplots(nrows=2, ncols=1, sharex='col', sharey=False,
+                    gridspec_kw={'height_ratios': [4, 1]})
+magplot = axes[0]
+coilplot = axes[1]
+coilplot.set_xlim(-1, 1)
 
 plt_canvas = FigureCanvasTkAgg(fig, root)
 plt_canvas.get_tk_widget().grid(row=0, column=0)
 
+magplot.plot(x := np.linspace(-1, 1, 50), np.sin(x))
 
-def plt_click(event):
-    print(event)
-    if event.inaxes == magplot:
-        magplot.plot(event.xdata, event.ydata, color='green', marker='x', linestyle='')
-        plt_canvas.draw()
+dots_xs = [
+        -.5,
+        .5
+        ]
+DOT_Y = 0
 
 
-fig.canvas.callbacks.connect('button_press_event', plt_click)
+def replot_dots(dots_xs):
+    coilplot.clear()
+    for dot_x in dots_xs:
+        coilplot.plot(dot_x, DOT_Y, color='red', marker='o', linestyle='')
+    plt_canvas.draw()
 
-COIL_CANVAS_HEIGHT = 20
-coil_canvas = Canvas(root, width=500, height=COIL_CANVAS_HEIGHT, background='gray75')
-coil_canvas.grid(column=0, row=1)
 
-DOT_SIZE = 10
-DOT_START_Y = (COIL_CANVAS_HEIGHT - DOT_SIZE) / 2
-coil_canvas.create_oval(0, DOT_START_Y, DOT_SIZE, DOT_START_Y + DOT_SIZE, fill='red', outline='blue', tags=('token',))
+replot_dots(dots_xs)
 
-coil_canvas.tag_bind("token", "<ButtonPress-1>", drag_start)
-coil_canvas.tag_bind("token", "<ButtonRelease-1>", drag_stop)
-coil_canvas.tag_bind("token", "<B1-Motion>", drag)
+fig.canvas.callbacks.connect('button_press_event', drag_start)
+fig.canvas.callbacks.connect('button_release_event', drag_stop)
+fig.canvas.callbacks.connect('motion_notify_event', drag)
 
 root.mainloop()
